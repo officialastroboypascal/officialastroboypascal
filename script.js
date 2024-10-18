e9364b7047df5512eec4a4d766dcf9289db96737
astroboy Pascal <shabaniwalukeba17@gmail.com>
2024-10-18 00:43:15 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    alert("Thank you for contacting Astroboy Pascal! We will get back to you soon.");
+});
